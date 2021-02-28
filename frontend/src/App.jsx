a3bc9b393b14e7e4a041948a307d25870cb708c6
Nabin Thapa <echonabin@gmail.com>
2021-02-28 15:21:38 +0545

    frontend init and some basic ui added with backend init

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { Footer, Header } from "./components";
+import { HomeScreen, ProductScreen } from "./screens";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello from the app</h1>
-    </div>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 };
 
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "./assets/bootstrap.min.css";
+import "./assets/main.css";
 
 ReactDOM.render(
   <React.StrictMode>
